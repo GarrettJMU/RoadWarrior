10cae52484cdd5fd7be51cb65c67b8b41057ab2e
GarrettJMU <garretthughes3@gmail.com>
2018-11-05 20:34:18 -0800

    Initial Commit

---
+++ RoadWarrior/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoadWarrior
+//
+//  Created by Garrett Hughes on 11/5/18.
+//  Copyright © 2018 Garrett Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
