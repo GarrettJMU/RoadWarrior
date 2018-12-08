deac303d617e748741b3a51fc628c8b637131cb9
GarrettJMU <garretthughes3@gmail.com>
2018-12-08 14:07:20 -0800

    add in functionality for gallery upload as well

--- RoadWarrior/ViewController.swift
+++ RoadWarrior/ViewController.swift
@@ -12,59 +12,19 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
 
     let imagePicker = UIImagePickerController()
     
-    
-    @IBAction func openCamera(_ sender: Any) {
-        presentCamera()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        promptPhoto()
     }
     
-    func presentCamera() {
+    @IBAction func openCamera(_ sender: Any) {
         imagePicker.sourceType = .camera
         self.present(imagePicker, animated: true)
     }
-
-    func promptPhoto() {
-        
-        let prompt = UIAlertController(title: "Choose a Photo",
-                                       message: "Please choose a photo.",
-                                       preferredStyle: .actionSheet)
-        
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        func presentCamera(_ _: UIAlertAction) {
-            imagePicker.sourceType = .camera
-            self.present(imagePicker, animated: true)
-        }
-        
-        let cameraAction = UIAlertAction(title: "Camera",
-                                         style: .default,
-                                         handler: presentCamera)
-        
-        func presentLibrary(_ _: UIAlertAction) {
-            imagePicker.sourceType = .photoLibrary
-            self.present(imagePicker, animated: true)
-        }
-        
-        let libraryAction = UIAlertAction(title: "Photo Library",
-                                          style: .default,
-                                          handler: presentLibrary)
-        
-        
-        let cancelAction = UIAlertAction(title: "Cancel",
-                                         style: .cancel,
-                                         handler: nil)
-        
-        prompt.addAction(cameraAction)
-        prompt.addAction(libraryAction)
-        prompt.addAction(cancelAction)
-        
-        self.present(prompt, animated: true, completion: nil)
+    
+    @IBAction func openGallery(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true)
     }
 
 }
