5140541c758f063aa7a725f37582509f1be5feda
GarrettJMU <garretthughes3@gmail.com>
2018-12-10 19:36:05 -0800

    Add in first of vision kit code

--- RoadWarrior/ViewController.swift
+++ RoadWarrior/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Vision
 
 class CameraViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -25,6 +26,66 @@ class CameraViewController: UIViewController,UIImagePickerControllerDelegate, UI
     @IBAction func openGallery(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
         self.present(imagePicker, animated: true)
+    }
+    
+    internal func imagePickerController(_ picker: UIImagePickerController,
+                                        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        // Display image on screen.
+//        show(originalImage)
+        
+        let cgOrientation = CGImagePropertyOrientation(rawValue: UInt32(Float(originalImage.imageOrientation.rawValue)))
+        
+        guard let cgImage = originalImage.cgImage else {
+            return
+        }
+        
+        performVisionRequest(image: cgImage,
+                             orientation: cgOrientation!)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func performVisionRequest(image: CGImage, orientation: CGImagePropertyOrientation) {
+        let requests = createVisionRequests()
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image,
+                                                        orientation: orientation,
+                                                        options: [:])
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try imageRequestHandler.perform(requests)
+            } catch let error as NSError {
+                print("Failed to perform image request: \(error)")
+//                self.presentAlert("Image Request Failed", error: error)
+                return
+            }
+        }
+    }
+    
+    fileprivate func createVisionRequests() -> [VNRequest] {        
+        var requests: [VNRequest] = []
+        
+        // Create & include a request if and only if switch is ON.
+        requests.append(self.textDetectionRequest)
+        
+        // Return grouped requests as a single array.
+        return requests
+    }
+    
+    lazy var textDetectionRequest: VNDetectTextRectanglesRequest = {
+        let textDetectRequest = VNDetectTextRectanglesRequest(completionHandler: self.handleDetectedText)
+        // Tell Vision to report bounding box around each character.
+        textDetectRequest.reportCharacterBoxes = true
+        return textDetectRequest
+    }()
+    
+    fileprivate func handleDetectedText(request: VNRequest?, error: Error?) {
+        if let nsError = error as NSError? {
+//            self.presentAlert("Text Detection Error", error: nsError)
+            return
+        }
+        print(request?.results, "this is the request.results")
     }
 
 }
