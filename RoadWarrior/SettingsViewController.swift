37e9c48e7f6062c794a8bd7510ced203810737d0
GarrettJMU <garretthughes3@gmail.com>
2018-12-15 13:15:06 -0800

    add in settings page

---
+++ RoadWarrior/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  RoadWarrior
+//
+//  Created by Garrett Hughes on 12/15/18.
+//  Copyright © 2018 Garrett Hughes. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
