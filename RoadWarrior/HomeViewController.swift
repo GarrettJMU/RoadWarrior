e709a830c2e8e1c6173ac0531cafa97875a71349
Rhythm Mecwan <rhythm813mecwan@gmail.com>
2018-12-19 23:49:27 -0500

    three pages are now onboarding and open after the launch storyboard

--- RoadWarrior/HomeViewController.swift
+++ RoadWarrior/HomeViewController.swift
@@ -14,4 +14,16 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "hasViewedWalkthrough") {
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
+        if let walkthroughViewController = storyboard.instantiateViewController(withIdentifier: "WalkthroughViewController") as? WalkthroughViewController {
+            present(walkthroughViewController, animated: true, completion: nil)
+        }
+    }
+    
 }
